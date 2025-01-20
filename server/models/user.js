b092669dcf348160919c3f5dd43b806024e56944
Khus19hi <114606931+Khus19hi@users.noreply.github.com>
2025-01-20 18:59:31 +0530

    Committed Successfully

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,26 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-}, {
-    timestamps: true,
-});
-
-module.exports = mongoose.model("User", userSchema);
+import mongoose from "mongoose";
+ const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        profilePic: {
+            type: String,
+            default: "",
+        },
+    },
+    { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
